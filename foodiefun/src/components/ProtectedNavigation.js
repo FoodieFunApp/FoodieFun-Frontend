10ff8b38cccb04f090c2dd61bd17668e3942cd54
Eric Hendrix <agentmrbig@gmail.com>
2019-09-10 22:55:34 -0400

    signup done

--- foodiefun/src/components/ProtectedNavigation.js
+++ foodiefun/src/components/ProtectedNavigation.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import AddReview from './AddReview.js';
+import Link from 'react';
 
-function ProtectedNavigation () {
-    return(
+function ProtectedNavigation() {
+    return (
         <div className="protected-navigation">
             <Link to="/dashboard">
                 <div className="logo-box">
-                    <img src="https://img.icons8.com/cotton/64/000000/sunny-side-up-eggs--v2.png" />
+                    <img alt="sunnysideupeggspic" src="https://img.icons8.com/cotton/64/000000/sunny-side-up-eggs--v2.png" />
                     <h1>FoodieFun</h1>
                 </div>
             </Link>
-            
+
             <div className="protected-navigation-links">
                 <AddReview />
             </div>
--- foodiefun/src/components/SignUp.js
+++ foodiefun/src/components/SignUp.js
@@ -1,25 +1,71 @@
 import React from 'react';
-import {Form, FormGroup, Label, Input, Button, FormText} from 'reactstrap';
+import { Form, FormGroup, Label, Input, Button, FormText } from 'reactstrap';
 import Navigation from './Navigation.js';
+import axios from 'axios';
 
-function SignUp () {
-    return(
-        <div className="sign-up">
-            <Navigation/>
-            <h1>Create Your Account To Get Started</h1>
-            <Form className="form sign-up-form">
-                <FormGroup>
-                    <Label>Username</Label>
-                    <Input type="string" name="username"/>
-                </FormGroup>
-                <FormGroup>
-                    <Label>Password</Label>
-                    <Input type="string" name="password"/>
-                </FormGroup>
-                <Button>Create Account</Button>
-            </Form>
-        </div>
-    )
+export default class SignUp extends React.Component {
+    // constructor(args) {
+    //     super(args);
+    //     this.state = {
+    //         username: '',
+    //         password: ''
+    //     }
+    // }
+    state = {
+        username: '',
+        password: ''
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+        const user = {
+            username: this.state.username,
+            password: this.state.password
+        }
+
+        axios
+            .post(`https://foodie-fun-backend.herokuapp.com/api/auth/register`, user)
+            .then(res => {
+                console.log(res);
+                console.log(res.data);
+            })
+    }
+
+    handleChange = event => {
+        this.setState({ [event.target.name]: event.target.value })
+    }
+
+    render() {
+        return (
+            <div className="sign-up">
+                <Navigation />
+                <h1>Create Your Account To Get Started</h1>
+                <Form className="form sign-up-form" onSubmit={this.handleSubmit}>
+                    <FormGroup>
+                        <Label>Username</Label>
+                        <Input
+                            type="text"
+                            name="username"
+                            placeholder="Username"
+                            value={this.state.username}
+                            onChange={this.handleChange}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label>Password</Label>
+                        <Input
+                            type="text"
+                            name="password"
+                            placeholder="Password"
+                            value={this.state.password}
+                            onChange={this.handleChange}
+                        />
+                    </FormGroup>
+                    <Button type="submit">Create Account</Button>
+                </Form>
+            </div>
+        )
+    }
 }
 
-export default SignUp;
