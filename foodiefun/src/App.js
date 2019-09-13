0922f3ac20d5c80f0fb861179d702fe34009dddd
Ken Ruf <ruf.kenichi@gmail.com>
2019-09-12 21:24:07 -0500

    more conflicts

--- foodiefun/src/App.js
+++ foodiefun/src/App.js
@@ -4,25 +4,14 @@ import SignUp from './components/SignUp.js';
 import Login from './components/Login.js';
 import UserDashboard from './components/UserDashboard.js';
 import HomePage from './components/HomePage.js';
-<<<<<<< HEAD
-import AddReview from './components/AddReview.js';
-=======
 import AddReview from './components/AddReview';
 import { ProtectedRoute } from './components/PrivateRoute';
->>>>>>> dadb867889ed20328609f6c58f3748921144b4ec
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <Route exact path="/" component={HomePage}/>
-      <Route exact path="/register" component={SignUp}/>
-      <Route exact path="/login" component={Login}/>
-      <Route exact path="/app" component ={UserDashboard}/>
-      <Route exact path="/add-review" component={AddReview}/>
-=======
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/register" component={SignUp} />
@@ -32,7 +21,6 @@ function App() {
         <Route exact path="*" component={() => "404 Not Found"} />
       </Switch>
 
->>>>>>> dadb867889ed20328609f6c58f3748921144b4ec
     </div>
   );
 }
